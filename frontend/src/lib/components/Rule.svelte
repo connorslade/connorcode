04a37c8a076e13b37b83da0dbcb972b3cd195358
Connor Slade <connor@connorcode.com>
2024-04-18 18:11:51 -0400

    Start on project page

--- frontend/src/lib/components/Rule.svelte
+++ frontend/src/lib/components/Rule.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	type Style = 'solid' | 'dashed';
+	type Thickness = 'normal' | 'thin';
 
 	export let style: Style = 'solid';
+	export let thickness: Thickness = 'normal';
 </script>
 
-<hr style:border={`1px ${style}`} />
+{#if thickness == 'normal'}
+	<hr style:border={`1px ${style}`} />
+{:else if thickness == 'thin'}
+	<hr style:border="none" style:border-bottom={`1px ${style}`} />
+{/if}
---
+++ frontend/src/routes/projects/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { GITHUB } from '$lib/consts';
+	import Link from '$lib/components/Link.svelte';
+	import Rule from '$lib/components/Rule.svelte';
+</script>
+
+<h1>Projects</h1>
+
+<p>
+	You can find almost all of my projects on my Github, <Link href={GITHUB}>Basicprogrammer10</Link>.
+	Some of my more interesting or complete projects will be put here, with articles explaining what
+	they do and how they work.
+</p>
+
+<Rule thickness="thin" />
+
+<h3>dm42</h3>
+
+<p>Free42 program compiler and some miscellaneous programs.</p>
+
+<span>Created July 16, 2023</span>
