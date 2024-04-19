fc5725987b995996b276e66830360d89cf1a1cc4
Connor Slade <connor@connorcode.com>
2024-04-19 17:08:46 -0400

    Smooth scroll internial links

--- frontend/src/lib/components/HtmlRenderer.svelte
+++ frontend/src/lib/components/HtmlRenderer.svelte
@@ -2,6 +2,8 @@
 	import { Html, isTag, Element } from 'html-svelte-parser';
 
 	import Admonition from './Admonition.svelte';
+	import InnerLink from './InnerLink.svelte';
+	import Link from './Link.svelte';
 
 	function process_node(node: Element): any {
 		if (node.name == 'div' && node.attribs['element'] == 'admonition') {
@@ -12,6 +14,25 @@
 					title: node.attribs['title']
 				}
 			};
+		} else if (node.name == 'a') {
+			let href = node.attribs['href'];
+			if (href.startsWith('#'))
+				return {
+					component: InnerLink,
+					props: {
+						href,
+						id: node.attribs['id'],
+						_class: node.attribs['class'],
+						aria_hidden: node.attribs['aria-hidden']
+					}
+				};
+			else if (!href.startsWith('/'))
+				return {
+					component: Link,
+					props: {
+						href
+					}
+				};
 		}
 	}
 
---
+++ frontend/src/lib/components/InnerLink.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	function handleClick(event: MouseEvent) {
+		event.preventDefault();
+
+		let target = event.target as HTMLAnchorElement;
+		let anchorId = new URL(target.href).hash.slice(1);
+		let anchor = document.getElementById(anchorId);
+		if (anchor) anchor.scrollIntoView({ behavior: 'smooth' });
+	}
+
+	export let href: string;
+
+	export let id: string | null = null;
+	export let _class: string | null = null;
+	export let aria_hidden: boolean = false;
+</script>
+
+<a {href} {id} class={_class} aria-hidden={aria_hidden} on:click={handleClick}>
+	<slot />
+</a>
