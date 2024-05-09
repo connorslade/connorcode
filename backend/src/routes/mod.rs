17418a6f6209924892f92efdc7bdc55e0b5b321a
Connor Slade <connor@connorcode.com>
2024-05-09 07:39:47 -0400

    Init analytics routes

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -2,10 +2,12 @@ use afire::Server;
 
 use crate::app::App;
 
+mod analytics;
 mod files;
 mod writing;
 
 pub fn attach(server: &mut Server<App>) {
+    analytics::attach(server);
     files::attach(server);
     writing::attach(server);
 }
