93adebea80e68d6a41cb83838490d300c47fd4c9
Connor Slade <connor@connorcode.com>
2024-12-19 23:12:36 -0500

    Add GLSL syntax highlighting support

--- frontend/src/lib/consts.ts
+++ frontend/src/lib/consts.ts
@@ -1,3 +1,3 @@
-export const VERSION: string = '3.0.0';
+export const VERSION: string = '3.0.1';
 export const GITHUB: string = 'https://github.com/connorslade';
 export const SITE_REPO: string = 'connorslade/connorcode';
